fa69c628b1f72e3071da90bfef1015cd7366fd55
Allan Wang <me@allanwang.ca>
2018-04-05 15:42:53 -0400

    Update AuthenticationInterceptor constructor

--- app/src/main/java/com/fastaccess/provider/rest/interceptors/AuthenticationInterceptor.kt
+++ app/src/main/java/com/fastaccess/provider/rest/interceptors/AuthenticationInterceptor.kt
@@ -1,37 +1,31 @@
 package com.fastaccess.provider.rest.interceptors
 
-import com.fastaccess.helper.Logger
 import com.fastaccess.helper.PrefGetter
 import com.fastaccess.provider.scheme.LinkParserHelper
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
 
-class AuthenticationInterceptor : Interceptor {
-    private var isScrapping: Boolean = false
-    private var token: String? = null
-    private var otp: String? = null
+class AuthenticationInterceptor @JvmOverloads constructor(
+        private val token: String? = null,
+        private val otp: String? = null,
+        private val isScrapping: Boolean = false
+) : Interceptor {
 
-    constructor(token: String? = null, otp: String? = null) {
-        this.token = token
-        this.otp = otp
-    }
+    constructor(isScrapping: Boolean) : this(null, isScrapping = isScrapping)
 
-    @JvmOverloads constructor(isScrapping: Boolean = false) {
-        this.isScrapping = isScrapping
-    }
-
-    @Throws(IOException::class) override fun intercept(chain: Interceptor.Chain): Response {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
         val builder = original.newBuilder()
         val isEnterprise = LinkParserHelper.isEnterprise(original.url().host())
         val authToken = if (token.isNullOrBlank()) if (isEnterprise) PrefGetter.getEnterpriseToken() else PrefGetter.getToken() else token
         val otpCode = if (otp.isNullOrBlank()) if (isEnterprise) PrefGetter.getEnterpriseOtpCode() else PrefGetter.getOtpCode() else otp
-        if (!authToken.isNullOrBlank()) {
-            builder.header("Authorization", if (authToken!!.startsWith("Basic")) authToken else "token " + authToken)
+        if (authToken?.isNotBlank() == true) {
+            builder.header("Authorization", if (authToken.startsWith("Basic")) authToken else "token $authToken")
         }
-        if (!otpCode.isNullOrBlank()) {
-            builder.addHeader("X-GitHub-OTP", otpCode!!.trim())
+        if (otpCode?.isNotBlank() == true) {
+            builder.addHeader("X-GitHub-OTP", otpCode.trim())
         }
         if (!isScrapping) builder.addHeader("User-Agent", "FastHub")
         val request = builder.build()
